ab8d942df5bb4f38f2e100d0a0af1bf702c6d15a
Chrines <chrinesskatete@gmail.com>
2026-02-25 13:03:53 +0200

    Register our services

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,5 +1,9 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddScoped<IPieRepository, MockPieRepository>(); // we use AddScoped to create a new instance of MockPieRepository for each HTTP request. This allows us to have a fresh instance of the repository for each request, which is useful for testing and development purposes.
+builder.Services.AddScoped<ICategoryRepository, MoCkCategoryRepository>(); // we use AddScoped to create a new instance of MockCategoryRepository for each HTTP request. This allows us to have a fresh instance of the repository for each request, which is useful for testing and development purposes.
 builder.Services.AddControllersWithViews(); // Add services to the container.
 
 var app = builder.Build();
